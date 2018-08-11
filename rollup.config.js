5603dc826c2429d37a21860a5189cd55c5da9947
Trevor Smith <trevorlynnsmith@gmail.com>
2018-08-11 13:30:55 -0600

    Make redux, redux-thunk, react-redux, and immer peer dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
 import babel from 'rollup-plugin-babel'
-import nodeResolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
-import { uglify } from 'rollup-plugin-uglify'
 
 const env = process.env.NODE_ENV
 const config = {
